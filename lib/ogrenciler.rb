7b783b6fec543e1141bdae0c0b8b65b628f4b884
Umurcan Görür <mail@ugorur.com>
2017-02-06 16:42:57 +0300

    first commit

---
+++ lib/ogrenciler.rb
@@ -0,0 +1,80 @@
+module Okul
+	
+	class Ogrenciler
+		attr_accessor :adi, :soyadi, :dogum_tarihi, :cinsiyet
+		attr_reader :id
+
+		DOSYA_ADI = 'db/ogrenciler.txt'
+
+		def save
+			ogrenci = [@id, @adi, @soyadi, @dogum_tarihi, @cinsiyet]
+
+			File.open(DOSYA_ADI, 'w') do |dosya|
+				dosya.puts ogrenci.join(';')
+			end
+		end
+
+		def add
+			auto_id
+			ogrenci = [@id, @adi, @soyadi, @dogum_tarihi, @cinsiyet]
+
+			File.open(DOSYA_ADI, 'a') do |dosya|
+				dosya.puts ogrenci.join(';')
+			end
+		end
+
+		private
+		def auto_id
+			@id = Ogrenciler.list.last.id.next
+			@id ||= 0
+		end
+
+		public
+		class << self
+
+			def one (id)
+				ogrenciler = list.select {|ogrenci| ogrenci.id == id}
+
+				ogrenciler[0]
+			end
+
+			def search(**params, &block) # params = {adi: 'Ahmet'}
+				if block_given?
+					list.select(&block)
+				else
+					_list = list()
+					params.each do |key, value|
+						_list.select! { |ogrenci| ogrenci.method(key).call == value }
+					end
+					_list
+				end
+			end
+
+			def list
+				dosya = File.open(DOSYA_ADI, "r") 
+				ogrenciler = dosya.readlines.delete_if {|a| a.empty?}.map do |ogrenci|
+					csv_row ogrenci
+				end 
+				dosya.close
+				ogrenciler
+			end
+
+			private
+			def csv_row(row)
+					row = row.split(';')
+
+					ogrenci = self.new
+					ogrenci.id = row[0].to_i
+					ogrenci.adi = row[1]
+					ogrenci.soyadi = row[2]
+					ogrenci.dogum_tarihi = Time.new(row[3])
+					ogrenci.cinsiyet = row[4]
+
+					ogrenci
+			end
+
+		end
+
+	end
+
+end
---
+++ program.rb
@@ -0,0 +1,11 @@
+require './lib/ogrenciler'
+
+ogrenci = Okul::Ogrenciler.new
+ogrenci.adi = "Ahmet"
+ogrenci.soyadi = "Balyali"
+ogrenci.dogum_tarihi = Time.new(2000, 12, 3)
+ogrenci.cinsiyet = "Erkek"
+
+ogrenci.add()
+
+puts "#{ogrenci.id} id ile ogrenci katyit oldu"
